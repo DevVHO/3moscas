4698195f75b65873f87c56ae8e16d69aa3b68002
DevVHO <161318358+DevVHO@users.noreply.github.com>
2025-05-28 05:12:25 +0100

    Sistema de instancias das células
    Tenho problema no posicionamento dessa grid.

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -1,14 +1,41 @@
 using Godot;
 using System;
+using System.Data;
 
 public partial class GameManager : Node
 {
-    [Export] public PackedScene Mosqueteiro; 
+    [Export] public PackedScene Cell;
+
+    public int rows = 5;
+    public int columns = 5;
+    private float cellSize = 108f;
+
+    private Node2D[,] grid;
     public override void _Ready()
     {
-        //Instancia do mosqueteiro
-        Node MosqueteiroIns = Mosqueteiro.Instantiate();
-        AddChild(MosqueteiroIns);
-    }
+        //Busca o tamanho da grid
 
+
+        grid = new Node2D[rows, columns];
+        GenerateGrid();
+    }
+    private void GenerateGrid()
+    {
+        for (int y = 0; y < rows; y++)
+        {
+            for (int x = 0; x < columns; x++)
+            {
+                Node2D cellInstance = Cell.Instantiate<Node2D>();
+
+                // Define a posição da célula na tela
+                cellInstance.Position = new Vector2(x * cellSize, y * cellSize);
+
+                // Adiciona à árvore como filha desse node
+                AddChild(cellInstance);
+
+                // Salva referência na matriz
+                grid[y, x] = cellInstance;
+            }
+        }
+    }
 }
