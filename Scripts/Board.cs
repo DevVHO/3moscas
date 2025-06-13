0c93ef03b83b92075227dc47f1d846f34dff67ad
DevVHO <161318358+DevVHO@users.noreply.github.com>
2025-06-13 17:02:20 +0100

    Update_NADA
    Quase Nada Mudou

--- Scripts/Board.cs
+++ Scripts/Board.cs
@@ -6,27 +6,18 @@ public partial class Board : Node2D
     public int rows = 5;
     public int columns = 5;
     private Node2D[,] grid;
+
+    
     [Export] public PackedScene cellScene;
-    [Export] public PackedScene guardaScene;
     public override void _Ready()
     {
         grid = new Node2D[rows, columns];
+        //Pegar a position de target para que assim eu consiga instanciar através dela
     }
 
     public void GenerateGrid()
     {
-        GD.Print($"GenerateGrid chamado. grid é {(grid == null ? "null" : "inicializado")}");
-        if (cellScene == null)
-        {
-            GD.PrintErr("cellScene está null!");
-            return;
-        }
         var temp = cellScene.Instantiate() as Node2D;
-        if (temp == null)
-        {
-            GD.PrintErr("cellScene não é Node2D.");
-            return;
-        }
 
         var sprite = temp.GetNode<Sprite2D>("Sprite2D");
         var size = sprite.Texture.GetSize();
@@ -37,21 +28,16 @@ public partial class Board : Node2D
         float gridHeight = rows * size.Y * scale.Y;
         Vector2 screenSize = GetViewport().GetVisibleRect().Size;
         Vector2 offset = (screenSize - new Vector2(gridWidth, gridHeight + 40)) / 2.65f;
-
+        
         for (int y = 0; y < rows; y++)
         {
             for (int x = 0; x < columns; x++)
             {
-                Vector2 pos = offset + new Vector2(x * size.X * scale.X, y * size.Y * scale.Y);
-
+                Vector2 pos =  offset + new Vector2(x * size.X * scale.X, y * size.Y * scale.Y);
                 var cell = cellScene.Instantiate<Node2D>();
                 cell.Position = pos;
                 AddChild(cell);
                 grid[y, x] = cell;
-
-                var guarda = guardaScene.Instantiate<Node2D>();
-                guarda.Position = pos;
-                AddChild(guarda);
             }
         }
     }
