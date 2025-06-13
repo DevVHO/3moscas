d7ea05021b65044fe621e250dc77081f8d41547f
DevVHO <161318358+DevVHO@users.noreply.github.com>
2025-06-13 16:00:41 +0100

    Update
    Vitooooooooo

---
+++ Scripts/Board.cs
@@ -0,0 +1,58 @@
+using Godot;
+using System;
+
+public partial class Board : Node2D
+{
+    public int rows = 5;
+    public int columns = 5;
+    private Node2D[,] grid;
+    [Export] public PackedScene cellScene;
+    [Export] public PackedScene guardaScene;
+    public override void _Ready()
+    {
+        grid = new Node2D[rows, columns];
+    }
+
+    public void GenerateGrid()
+    {
+        GD.Print($"GenerateGrid chamado. grid é {(grid == null ? "null" : "inicializado")}");
+        if (cellScene == null)
+        {
+            GD.PrintErr("cellScene está null!");
+            return;
+        }
+        var temp = cellScene.Instantiate() as Node2D;
+        if (temp == null)
+        {
+            GD.PrintErr("cellScene não é Node2D.");
+            return;
+        }
+
+        var sprite = temp.GetNode<Sprite2D>("Sprite2D");
+        var size = sprite.Texture.GetSize();
+        var scale = temp.Scale;
+        temp.QueueFree();
+
+        float gridWidth = columns * size.X * scale.X;
+        float gridHeight = rows * size.Y * scale.Y;
+        Vector2 screenSize = GetViewport().GetVisibleRect().Size;
+        Vector2 offset = (screenSize - new Vector2(gridWidth, gridHeight + 40)) / 2.65f;
+
+        for (int y = 0; y < rows; y++)
+        {
+            for (int x = 0; x < columns; x++)
+            {
+                Vector2 pos = offset + new Vector2(x * size.X * scale.X, y * size.Y * scale.Y);
+
+                var cell = cellScene.Instantiate<Node2D>();
+                cell.Position = pos;
+                AddChild(cell);
+                grid[y, x] = cell;
+
+                var guarda = guardaScene.Instantiate<Node2D>();
+                guarda.Position = pos;
+                AddChild(guarda);
+            }
+        }
+    }
+}
--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -1,81 +1,10 @@
 using Godot;
-using System;
-using System.Data;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks.Dataflow;
 
-public partial class GameManager : Node
+public partial class GameManager : Node2D
 {
-    [Export] public PackedScene Cellscene;
-    [Export] public PackedScene Guarda;
-    [Export] public PackedScene Mosca;
-
-    public enum EstadoJogo { JOGANDO = 0, GANHOU_G = 1, GANHOU_M = 2 };
-
-    public Vector2 CellSize;
-    private Vector2 CellScale;
-    private Vector2 Offset;
-    public int rows = 5;
-    public int columns = 5;
-    private Node2D[,] grid;
     public override void _Ready()
     {
-        //Instancia temporária para adquirir o tamanho do Sprite
-        grid = new Node2D[rows, columns];
-        GenerateGrid();
-    }
-    private void GenerateGrid()
-    {
-        Node2D cellTemp = Cellscene.Instantiate<Node2D>();
-        Sprite2D spritecell = cellTemp.GetNode<Sprite2D>("Sprite2D");
-        Vector2 CellSize = spritecell.Texture.GetSize();
-        Vector2 CellScale = spritecell.Scale;
-        cellTemp.QueueFree();
-        //Tamanho da grid
-        float gridWidth = columns * CellSize.X * CellScale.X;
-        float gridHeight = rows * CellSize.Y * CellScale.Y;
-        Vector2 screenSize = GetViewport().GetVisibleRect().Size;
-        Vector2 Offset = (screenSize - new Vector2(gridWidth, gridHeight + 40)) / 2.65f;
-
-
-        for (int y = 0; y < rows; y++)
-        {
-            for (int x = 0; x < columns; x++)
-            {
-                Node2D cellInstance = Cellscene.Instantiate<Node2D>();
-                // Define a posição da célula na tela
-                Vector2 pos = Offset + new Vector2(x * CellSize.X * CellScale.X, y * CellSize.Y * CellScale.Y);
-                cellInstance.Position = pos;
-                GD.Print(screenSize);
-
-                // Adiciona à árvore como filha desse node
-                AddChild(cellInstance);
-
-                // Salva referência na matriz
-                grid[y, x] = cellInstance;
-
-
-                //Cria  Guardas
-                 Node2D guardainst = Guarda.Instantiate<Node2D>();
-                // Define a posição da célula na tela
-                Vector2 pos1 = Offset + new Vector2(x * CellSize.X * CellScale.X, y * CellSize.Y * CellScale.Y);
-                guardainst.Position = pos1;
-                GD.Print(screenSize);
-
-                // Adiciona à árvore como filha desse node
-                AddChild(guardainst);
-
-                // Salva referência na matriz
-                grid[2, 2] = guardainst;
-            }
-
-        }
-    }
-    
-    public void GerarPeca()
-    {
-        
+        var board = GetParent().GetNode<Board>("Gridmanager");
+        board.GenerateGrid();
     }
 }
-    
